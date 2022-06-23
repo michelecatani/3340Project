51fd4a781334e667e1aada245d10aea10776fff5
michelecatani <catani@uwindsor.ca>
2022-06-22 21:07:25 -0400

    updated with react front end

---
+++ back-end/flaskServer/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+from flask_login import  current_user
+
+# code for creating "routes"
+
+views = Blueprint('views', __name__)
+
+# name of our blue print
+
+@views.route('/')
+def home():
+    return render_template("home.html", user=current_user)
---
+++ back-end/run.py
@@ -0,0 +1,6 @@
+from .flaskServer import create_app
+
+app = create_app()
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
