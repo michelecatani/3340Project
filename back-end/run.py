1ea0cef4ced0001801443d22b3389d212f07394d
michelecatani <catani@uwindsor.ca>
2022-06-22 21:45:21 -0400

    SDFasdfasdfas

--- back-end/run.py
+++ back-end/run.py
@@ -1,4 +1,4 @@
-from .flaskServer import create_app
+from flaskServer import create_app
 
 app = create_app()
  
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./index.css";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+
+  <Router>
+  <Navigation />
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+  <Footer />
+  </Router>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
