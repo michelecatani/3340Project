f924cea31e00c290b9cc761e82d01a1c9f623c99
NajiaSb <93660672+NajiaSb@users.noreply.github.com>
2022-07-28 16:47:57 -0400

    Items Page Update

--- front-end/pages/items/items.js
+++ front-end/pages/items/items.js
@@ -3,14 +3,15 @@ Right now it's attached to an endpoint that just returns basic items, but that o
 
 import React from "react";
 import { useState, useEffect } from "react";
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import convertToImage from '../../src/utils/base64';
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import convertToImage from "../../src/utils/base64";
+import Grid from "@mui/material/Grid";
 
 export default function Items() {
   const [data, setData] = useState([{}]);
@@ -28,164 +29,157 @@ export default function Items() {
   clickable... material ui components can be used, basically just better looking and increased functionality.
   */
   return (
-    <Container
-      sx={{ 
-        maxWidth: 600, 
-        width: "100%", 
-        justifyContent: "center",
-        alignItems: "center", 
-        margin: "auto",
-        border: 1, 
-        borderColor: '#1D1B27'
-      }}
-    >
-
-      <Button 
-        href="/items/createItem"
-        >
-        Create an Item
-      </Button>
-
-      <h1><i>NEOBAY CATALOGUE</i></h1>
-
-      {
-        typeof data === "undefined" ?  (<p><i>Loading...</i></p>) : (data.map((item, i) => 
-          
-          <Container>
-            <Box 
-              sx={{ 
-                  maxWidth: 600, 
-                  width: "100%", 
-                  justifyContent: "center",
-                  alignItems: "center", 
-                  margin: "auto",
-                  border: 1, 
-                  borderColor: '#1D1B27'
-              }}
+    <div style={{ padding: "3%" }}>
+      <Container
+        sx={{
+          maxWidth: 200,
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "auto",
+        }}
+        container
+      >
+        <Box>
+          <Grid container justifyContent="space-between" sx={{ mb: 10 }}>
+            <Typography
+              color="black"
+              variant="h4"
+              style={{ fontWeight: "bold" }}
             >
-
-              <Typography variant="h4" align="center">
-                  <img src={convertToImage(item.image_file)} alt="NeoBay" width="500px"/>
-              </Typography>
-
-            </Box>
-            
-            <h3 key={i}> {item.name} </h3>
-            <Button href={`/items/${item.id}`}>
-              See Details
+              NeoBay Catalogue
+            </Typography>
+            <Button variant="outlined" href="/items/createItem">
+              Create an Item
             </Button>
+          </Grid>
+        </Box>
+        <Box>
+          {typeof data === "undefined" ? (
+            <p>
+              <i>Loading...</i>
+            </p>
+          ) : (
+            data.map((item, i) => (
+              <Container>
+                <Box
+                  sx={{
+                    maxWidth: 600,
+                    width: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: "auto",
+                    border: 1,
+                    borderColor: "#1D1B27",
+                  }}
+                >
+                  <Typography variant="h4" align="center">
+                    <img
+                      src={convertToImage(item.image_file)}
+                      alt="NeoBay"
+                      width="500px"
+                    />
+                  </Typography>
+                </Box>
 
-          </Container>
+                <h3 key={i}> {item.name} </h3>
+                <Button href={`/items/${item.id}`}>See Details</Button>
+              </Container>
+            ))
+          )}
+        </Box>
+        <Box>
+          <Grid item>
+            {itemData.map((item) => (
+              <ImageListItem
+                sx={{ m: 2, border: 1, borderColor: "#1D1B27" }}
+                key={item.img}
+              >
+                <img
+                  src={`${item.img}?w=248&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading="lazy"
+                />
 
-        ))
-      }
+                <ImageListItemBar
+                  title={item.title}
+                  subtitle={<span>User: {item.author}</span>}
+                  position="below"
+                />
 
-    <ImageList 
-      sx={{ 
-        maxWidth: 600, 
-        height: 'vh',
-        alignItems: "center",
-        justifyContent: "center",
-        margin: 'auto',
-        border: 1,
-        borderColor: '#1D1B27'
+                <Button href={`/items/${item.id}`}>See Details</Button>
 
-      }}>
-
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-
-          <ImageListItemBar
-            title={item.title}
-            subtitle={<span>User: {item.author}</span>}
-            position="below"
-          />
-
-            <Button href={`/items/${item.id}`}>
-              See Details
-            </Button>
-
-            <br/><br/>
-
-        </ImageListItem>
-
-      ))}
-    </ImageList>
-
-    </Container>
-
-
+                <br />
+                <br />
+              </ImageListItem>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+    </div>
   );
-
 }
-
-
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "@bkristastucchio",
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+    author: "@rollelflex_graphy726",
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+    author: "@helloimnik",
   },
   {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+    author: "@nolanissac",
   },
   {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+    author: "@hjrc33",
   },
   {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
   },
   {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+    author: "@tjdragotta",
   },
   {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+    author: "@katie_wasserman",
   },
   {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+    author: "@silverdalex",
   },
   {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+    author: "@shelleypauls",
   },
   {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+    author: "@peterlaster",
   },
   {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+    author: "@southside_customs",
   },
 ];
